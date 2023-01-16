e3c34b8b4b8380268499de40d408c00b1b98e6de
HarshSaraiya11 <harshsaraiya11@gmail.com>
2023-01-16 15:04:11 +0530

    Final commit

--- insurance/home/urls.py
+++ insurance/home/urls.py
@@ -5,6 +5,7 @@ from home import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
+    path('faq', views.faq, name='faq'),
     path('about', views.about, name='about'),
     path('login', views.handleLogin, name='handleLogin'),
     path('signup', views.signup, name='signup'),
--- insurance/home/views.py
+++ insurance/home/views.py
@@ -15,6 +15,9 @@ def contact(request):
 def about(request):
     return render(request, 'home/about.html')
 
+def faq(request):
+    return render(request, 'home/faq.html')
+
 
 
 def signup(request):
